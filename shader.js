1f5ef0770beab442f6272fea817cfa61e2f4d073
Olav Salhus <osa@live.no>
2022-02-25 21:28:33 +0100

    Working webgl2 prototype quad with slider

--- shader.js
+++ shader.js
@@ -1,25 +1,120 @@
-let vs = `#version 300 es
-
+let vertexShaderSource = `#version 300 es
+precision highp float;
 in vec4 a_position;
 
+out vec2 color;
+
 void main() {
-  gl_position = a_position;
+  color = a_position.xy;
+  gl_Position = a_position;
 }
 `
 
-let fs = `#version 300 es
-
+let fragmentShaderSource = `#version 300 es
+precision highp float;
 out vec4 outColor;
-
+uniform float slider;
+in vec2 color;
 void main() {
-  outColor = vec4(1, 0, 1, 1);
+  outColor = vec4(color, slider, 1);
 }
 `
 
-function createShader() {
+function createShader(gl, type, source) {
+
+  var shader = gl.createShader(type)
+  gl.shaderSource(shader, source)
+  gl.compileShader(shader)
+
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
+  if (success) return shader
+
+  console.log(gl.getShaderInfoLog(shader))
+  gl.deleteShader(shader)
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) return program;
+
+  console.log(gl.getProgramInfoLog(program));
+
+  gl.deleteProgram(program);
+}
+
+let sliders;
+
+window.onload = function() {
+  let canvas = document.getElementById("canvas")
+
+
+  sliders = [document.getElementById("slider-1")];
+  
+  let gl = canvas.getContext("webgl2")
+  if (!gl) {
+    alert("Sorry, you need web gl 2");
+    return
+  }
+
+  let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+  var program = createProgram(gl, vertexShader, fragmentShader);
+
+  var positionAttributelocation = gl.getAttribLocation(program, "a_position")
+  console.log(positionAttributelocation)
+  var positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  var positions = [
+    -1, -1,
+    -1, +1,
+    +1, +1,
+    -1, -1,
+    +1, -1,
+    +1, +1,
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  var vao = gl.createVertexArray();
+
+  gl.bindVertexArray(vao);
+  gl.enableVertexAttribArray(positionAttributelocation);
+  
+  var size = 2;
+  var type = gl.FLOAT;
+  var normalize = false;
+  var stride = 0;
+  var offset = 0;
+  gl.vertexAttribPointer(positionAttributelocation, size, type, normalize, stride, offset);
+
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.useProgram(program);
+
+  gl.bindVertexArray(vao);
+
+
+  
+  requestAnimationFrame((time) => render(gl, program, time));
   
 }
 
-let canvas = document.createElement("canvas")
-let webgl2 = canvas.getContext("webgl2")
+function render(gl, program, time) {
 
+  var primitiveType = gl.TRIANGLES;
+  var offset = 0;
+  var count = 6;
+  var slider1Value = new Number(sliders[0].value)
+  var sliderUniformLocation = gl.getUniformLocation(program, "slider");
+  gl.uniform1f(sliderUniformLocation, slider1Value);
+  gl.drawArrays(primitiveType, offset, count);
+  requestAnimationFrame((time) => render(gl, program, time));
+}
